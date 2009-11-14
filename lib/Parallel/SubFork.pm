e16cc9c06f2498bdc02b221e52d6a74fb234bd3b
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-11-14 13:03:06 +0100

    Version change to 0.08

--- lib/Parallel/SubFork.pm
+++ lib/Parallel/SubFork.pm
@@ -83,7 +83,7 @@ __PACKAGE__->mk_accessors(
 
 
 # Version of the module
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 
 =head1 FUNCTIONS
--- lib/Parallel/SubFork/Task.pm
+++ lib/Parallel/SubFork/Task.pm
@@ -142,7 +142,7 @@ __PACKAGE__->mk_accessors(
 
 
 # Version of the module
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 
 # Check if it's possible to use a high precision alarm
